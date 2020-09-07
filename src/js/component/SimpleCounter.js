1280ec7790e09c58e994b93b939574e85e2b4555
OmarElFakih <omarelfakih97@gmail.com>
2020-09-07 15:57:45 +0000

    terminado

--- src/js/component/SimpleCounter.js
+++ src/js/component/SimpleCounter.js
@@ -4,20 +4,29 @@ import PropTypes from "prop-types";
 
 export const SimpleCounter = props => {
 	return (
-		<div className="row d-flex justify-content-around counter">
-			<div className="col-1 number-container">clock</div>
+		<div className="row d-flex justify-content-center counter">
+			<div className="col-1 number-container clock">
+				<i className="far fa-clock fa-xs"></i>
+			</div>
+			<li className="li-buffers"></li>
+			<li className="li-buffers"></li>
 			<React.Fragment>
 				<NumberContainer
 					digit={Math.floor(props.seconds / 100000) % 10}
 				/>
+				<li className="li-buffers"></li>
 				<NumberContainer
 					digit={Math.floor(props.seconds / 10000) % 10}
 				/>
+				<li className="li-buffers"></li>
 				<NumberContainer
 					digit={Math.floor(props.seconds / 1000) % 10}
 				/>
+				<li className="li-buffers"></li>
 				<NumberContainer digit={Math.floor(props.seconds / 100) % 10} />
+				<li className="li-buffers"></li>
 				<NumberContainer digit={Math.floor(props.seconds / 10) % 10} />
+				<li className="li-buffers"></li>
 				<NumberContainer digit={Math.floor(props.seconds % 10)} />
 			</React.Fragment>
 		</div>
